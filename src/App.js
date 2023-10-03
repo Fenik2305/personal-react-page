51238975b89e1bad8815eed6ca470a8ac30cefb1
Mykola_Fedorchenko <fenik2305@gmail.com>
2023-10-03 14:43:04 +0300

    deleted useless files and dirs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom'
-import GeneralInfo from './components/HomePage/GeneralInfo/GeneralInfo';
-import GeneralInfoStyles from './components/HomePage/GeneralInfo/GeneralInfo.css';
-import AboutMe from './components/AboutMePage/AboutMe/AboutMe';
-import AboutMeStyles from './components/AboutMePage/AboutMe/AboutMe.css';
+import GeneralInfo from './components/HomePage/GeneralInfo';
+import GeneralInfoStyles from './components/HomePage/GeneralInfo.css';
+import AboutMe from './components/AboutMePage/AboutMe';
+import AboutMeStyles from './components/AboutMePage/AboutMe.css';
+import Contacts from './components/ContactsPage/Contacts';
+import ContactsStyles from './components/ContactsPage/Contacts.css';
 import Header from './components/Header/Header';
 import HeaderStyles from './components/Header/Header.css';
 import Footer from './components/Footer/Footer';
 import FooterStyles from './components/Footer/Footer.css';
-import Contacts from './components/ContactsPage/Contacts/Contacts';
 
 function App() {
     const [messages, setMessages] = useState([]);
-    
+
     return (
       <div className='App'>
         <Header messagesNum={messages.length}/>
