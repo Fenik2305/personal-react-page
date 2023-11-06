54ccf6a14607e9eebdd8682e0533488894f2b880
Mykola_Fedorchenko <fenik2305@gmail.com>
2023-11-06 18:15:48 +0200

    Header badge fix

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom"
-import { Badge, Button, ButtonBase } from "@mui/material";
+import { Badge, Button } from "@mui/material";
 import { useLogout } from "../../hooks/useLogout";
 import { useMessagesContext } from "../../hooks/useMessagesContext";
 import { useAuthContext } from "../../hooks/useAuthContext";
@@ -53,7 +53,7 @@ export default function Header() {
                     Log out
                 </Button>)}
               
-              {user && (<Badge badgeContent={messages ? messages.length : 0} color='error'>
+              {user && (<Badge badgeContent={messages.length > 0 ? messages.length : 0} color='error'>
                   <Link to='/messages'>
                     <img className="UserActionsElement" src="/icons/mail-icon.png" alt="mail-icon" height={35} width={35}></img>
                   </Link>
